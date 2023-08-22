d8cd6099c112f27f722d1f51b1e0b7dbd3ab6024
marclopezsoler <marcls.arbucies@gmail.com>
2023-08-22 20:44:07 +0200

    styling nav

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,7 +3,7 @@ import styles from "@/public/styles/components/Header.module.scss";
 
 export default function Header() {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <Link href="/" className={styles.menu_item}>home</Link>
       <Link href="/work" className={styles.menu_item}>work</Link>
       <Link href="/about" className={styles.menu_item}>about</Link>
