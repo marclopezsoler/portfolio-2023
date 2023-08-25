09e42935006fbcb196ae3c2dd83a4c6653786953
marclopezsoler <marcls.arbucies@gmail.com>
2023-08-25 18:35:33 +0200

    mobile work

--- app/_data/data.js
+++ app/_data/data.js
@@ -1,75 +1,109 @@
 const works = [
-   {
-     num: '1',
-     id: 'work1',
-     title: 'project 1',
-     type: 'uni',
-     class: '3d'
-   },
-   {
-     num: '2',
-     id: 'work2',
-     title: 'project 2',
-     type: 'uni',
-     class: 'design'
-   },
-   {
-    num: '3',
-    id: 'work3',
-    title: 'project 3',
-    type: 'uni',
-    class: 'frontend'
+  {
+    num: "1",
+    id: "work1",
+    title: "project 1",
+    type: "uni",
+    class: "3d",
   },
   {
-    num: '4',
-    id: 'work4',
-    title: 'project 4',
-    type: 'personal',
-    class: 'photography'
-  },
-   {
-     num: '5',
-     id: 'work5',
-     title: 'project 5',
-     type: 'personal',
-     class: 'seo'
-   },
-   {
-     num: '6',
-     id: 'work6',
-     title: 'project 6',
-     type: 'personal',
-     class: 'social media'
-   },
-   {
-    num: '7',
-    id: 'work7',
-    title: 'project 7',
-    type: 'personal',
-    class: 'developing'
+    num: "2",
+    id: "work2",
+    title: "project 2",
+    type: "uni",
+    class: "design",
   },
   {
-    num: '8',
-    id: 'work8',
-    title: 'project 8',
-    type: 'work',
-    class: 'design'
+    num: "3",
+    id: "work3",
+    title: "long title project test",
+    type: "uni",
+    class: "frontend",
   },
   {
-    num: '9',
-    id: 'work9',
-    title: 'project 9',
-    type: 'work',
-    class: 'social media'
+    num: "4",
+    id: "work4",
+    title: "project 4",
+    type: "personal",
+    class: "photography",
   },
   {
-   num: '0',
-   id: 'work10',
-   title: 'project 10',
-   type: 'work',
-   class: '3d'
- },
- ];
- 
- export default works;
- 
+    num: "5",
+    id: "work5",
+    title: "project 5",
+    type: "personal",
+    class: "seo",
+  },
+  {
+    num: "6",
+    id: "work6",
+    title: "project 6",
+    type: "personal",
+    class: "social media",
+  },
+  {
+    num: "7",
+    id: "work7",
+    title: "project 7",
+    type: "personal",
+    class: "developing",
+  },
+  {
+    num: "8",
+    id: "work8",
+    title: "project 8",
+    type: "work",
+    class: "design",
+  },
+  {
+    num: "9",
+    id: "work9",
+    title: "project 9",
+    type: "work",
+    class: "social media",
+  },
+  {
+    num: "5",
+    id: "work5",
+    title: "project 5",
+    type: "personal",
+    class: "seo",
+  },
+  {
+    num: "6",
+    id: "work6",
+    title: "project 6",
+    type: "personal",
+    class: "social media",
+  },
+  {
+    num: "7",
+    id: "work7",
+    title: "project 7",
+    type: "personal",
+    class: "developing",
+  },
+  {
+    num: "8",
+    id: "work8",
+    title: "project 8",
+    type: "work",
+    class: "design",
+  },
+  {
+    num: "9",
+    id: "work9",
+    title: "project 9",
+    type: "work",
+    class: "social media",
+  },
+  {
+    num: "0",
+    id: "work10",
+    title: "project 10",
+    type: "work",
+    class: "3d",
+  },
+];
+
+export default works;
--- app/work/WorkPage.js
+++ app/work/WorkPage.js
@@ -6,6 +6,7 @@ import works from "../_data/data";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import Head from "next/head";
+import { isMobile } from "react-device-detect";
 
 export const metadata = {
   title: "work | marc lópez portfolio",
@@ -47,57 +48,92 @@ const WorkPage = () => {
         <link rel="icon" href="../icon.ico" type="image/x-icon" />
       </Head>
       <div className={styles.main}>
-        <p>WORK</p>
-        <section className={styles.work_content}>
-          <section className={styles.tags_parent}>
-            <div className={styles.work_tags}>
-              <a href="#uni">uni</a>
-              <a href="#personal">personal</a>
-              <a href="#work">work</a>
-            </div>
-          </section>
-          <section className={styles.work_items}>
-            {works.map((work) => {
-              return (
-                <button
-                  className={`${styles.workItem} ${
-                    hoveredItemId === work.id ? styles.opacity : ""
-                  }`}
-                  key={work.id}
-                  onMouseEnter={() => setHoveredItemId(work.id)}
-                  onMouseLeave={() => setHoveredItemId(null)}
-                >
-                  <div className={styles.title_name}>
-                    <span>{work.class}</span>
-                    <span className={styles.space}> / </span>
+        {!isMobile ? (
+          <section className={styles.work_content}>
+            <section className={styles.tags_parent}>
+              <div className={styles.work_tags}>
+                <a href="#uni">uni</a>
+                <a href="#personal">personal</a>
+                <a href="#work">work</a>
+              </div>
+            </section>
+            <section className={styles.work_items}>
+              {works.map((work) => {
+                return (
+                  <button
+                    className={`${styles.workItem} ${
+                      hoveredItemId === work.id ? styles.opacity : ""
+                    }`}
+                    key={work.id}
+                    onMouseEnter={() => setHoveredItemId(work.id)}
+                    onMouseLeave={() => setHoveredItemId(null)}
+                  >
                     <Link
                       href={`/work/${work.id}`}
                       className={styles.link}
-                      id={`${work.type} ${work.id}`}
+                      id={work.type}
                     >
-                      <p className={styles.title}>{work.title}</p>
+                      <p className={styles.title}>
+                        <span>{work.class}</span>
+                        <span className={styles.space}>/</span>
+                        {work.title}
+                      </p>
                     </Link>
-                  </div>
-                  <Image
-                    src={`/assets/images/work/${work.id}/image1.jpg`}
-                    width={100}
-                    height={100}
-                    className={styles.image}
-                    style={{
-                      left: `${
-                        work.num % 2 != 0
-                          ? localX - 220 + "px"
-                          : localX + 20 + "px"
-                      }`,
-                      top: localY - 50 + "px",
-                    }}
-                    alt={work.title}
-                  />
-                </button>
-              );
-            })}
+                    <Image
+                      src={`/assets/images/work/${work.id}/image1.jpg`}
+                      width={100}
+                      height={100}
+                      className={styles.image}
+                      style={{
+                        left: `${
+                          work.num % 2 != 0
+                            ? localX - 220 + "px"
+                            : localX + 20 + "px"
+                        }`,
+                        top: localY - 50 + "px",
+                      }}
+                      alt={work.title}
+                    />
+                  </button>
+                );
+              })}
+            </section>
           </section>
-        </section>
+        ) : (
+          <section className={styles.work_content_mobile}>
+            <section className={styles.tags_parent_mobile}>
+              <a href="#uni">uni</a>
+              <a href="#personal">personal</a>
+              <a href="#work">work</a>
+            </section>
+            <section className={styles.work_items_mobile}>
+              {works.map((work) => {
+                return (
+                  <button
+                    className={`${styles.workItem_mobile} ${
+                      hoveredItemId === work.id ? styles.opacity : ""
+                    }`}
+                    key={work.id}
+                    onMouseEnter={() => setHoveredItemId(work.id)}
+                    onMouseLeave={() => setHoveredItemId(null)}
+                  >
+                    <Link
+                      href={`/work/${work.id}`}
+                      className={styles.link}
+                      id={work.type}
+                    >
+                      <p className={styles.title}>
+                        <span>{work.class}</span>
+                        <span className={styles.space}>/</span>
+                        {work.title}
+                      </p>
+                    </Link>
+                  </button>
+                );
+              })}
+            </section>
+          </section>
+        )}
       </div>
     </motion.div>
   );
