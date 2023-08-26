af283a4e029c87da57f37c1492b35b7beefff4c4
marclopezsoler <marcls.arbucies@gmail.com>
2023-08-27 00:46:07 +0200

    intro title

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,23 @@ import Header from "./components/Header";
 import "./globals.scss";
 import Cursor from "./components/Cursor";
 import Popup from "./components/Popup";
+import { useEffect, useState } from "react";
 
 const RootLayout = ({ children }) => {
+  const [loads, setLoads] = useState(0);
+
+  useEffect(() => {
+    let count = sessionStorage.getItem("name");
+    if (count === null) {
+      count = 1;
+    } else {
+      count = Number(count) + 1;
+    }
+    sessionStorage.setItem("name", count);
+
+    setLoads(count);
+  }, []);
+
   return (
     <html lang="en" className="main">
       <Head>
@@ -19,10 +34,14 @@ const RootLayout = ({ children }) => {
         />
         <meta name="theme-color" content="#020b55" />
         <link rel="manifest" href="./manifest.webmanifest" />
-        <link rel="icon" href="./icon.ico" type="image/x-icon"/>
+        <link rel="icon" href="./icon.ico" type="image/x-icon" />
       </Head>
       <body>
         <section className="content">
+          <div className={`intro_title ${loads >= 1 ? 'animate_bg' : 'hide'}`}>
+            <p className={loads === 1 ? 'animate_title1' : 'hide'}>marc</p>
+            <p className={loads === 1 ? 'animate_title2' : 'hide'}>lópez</p>
+          </div>
           <Header />
           {isMobile ? <></> : <Cursor />}
           {isMobile ? <Popup /> : <></>}
