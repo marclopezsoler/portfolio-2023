0007db122e0003b9c90d435629500d92b898a39d
marclopezsoler <marcls.arbucies@gmail.com>
2023-08-29 02:19:57 +0200

    animations bugs

--- app/work/[id]/WorkDetailPage.js
+++ app/work/[id]/WorkDetailPage.js
@@ -45,6 +45,7 @@ export default function WorkDetailPage({ params }) {
           src={`/assets/images/work/${work.id}/image1.jpg`}
           width={200}
           height={100}
+          priority
           alt={`Image for ${work.title}`}
         />
       </div>
