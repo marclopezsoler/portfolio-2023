7bc8d75e0335b24b33638358647a665addd8e673
marclopezsoler <marcls.arbucies@gmail.com>
2023-08-31 02:05:40 +0200

    work detail page base responsive

--- app/components/NextWork.js
+++ app/components/NextWork.js
@@ -4,7 +4,7 @@ import styles from "@/public/styles/components/NextWork.module.scss";
 export default function NextWork ({nextWork}) {
    return(
       <div className={styles.main}>
-         <p className={styles.text}>like what you are seeing?<br></br>take a look to another work <span className={styles.link_parent}><Link href={`/work/work${nextWork}`} className={styles.link}>here</Link>!<span className={styles.finger}>👈</span></span></p>
+         <p className={styles.text}>like what you're seeing?<br></br>take a look to another work <span className={styles.link_parent}><Link href={`/work/work${nextWork}`} className={styles.link}>here</Link>!<span className={styles.finger}>👈</span></span></p>
       </div>
    )
 }
