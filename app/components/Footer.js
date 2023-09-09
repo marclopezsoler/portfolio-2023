be1612f6f57cee4c7763e2468fd1e192904822eb
marclopezsoler <marcls.arbucies@gmail.com>
2023-09-10 01:30:40 +0200

    cv updated

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -13,7 +13,7 @@ export default function Footer() {
         <a href="https://github.com/marclopezsoler" target="_blank" className={styles.footer_link}>Github</a>
         <a href="https://www.instagram.com/lopezsmarc/" target="_blank" className={styles.footer_link}>Instagram</a>
         <a href="https://twitter.com/marclopezsoler" target="_blank" className={styles.footer_link}>Twitter</a>
-          <a href="https://files.marclopez.xyz/CV_2023_MarcLopez.pdf" target="_blank" className={styles.footer_link}>CV</a>
+          <a href="https://files.marclopez.xyz/CV_MarcLopez.pdf" target="_blank" className={styles.footer_link}>CV</a>
       </section>
       <section className={styles.footer_row}>
         <span className={styles.footer_copy}>© 2023 Portfolio by Marc López</span>
