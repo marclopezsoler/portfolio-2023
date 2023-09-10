aeb3ca5330078a59638008c862535e04e8210e8d
marclopezsoler <marcls.arbucies@gmail.com>
2023-09-11 00:18:55 +0200

    ga

--- app/layout.js
+++ app/layout.js
@@ -33,6 +33,21 @@ const RootLayout = ({ children }) => {
         <noscript>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap" />
         </noscript>
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-CYPLVVSN8B"/>
+        <Script
+          id='google-analytics'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CYPLVVSN8B', {
+                page_path: window.location.pathname,
+              });
+            `,
+            }}
+        />
       </Head>
 
       <body>
