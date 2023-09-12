37eb99818908c29171e8a02bbc8d7505f80148f4
marclopezsoler <marcls.arbucies@gmail.com>
2023-09-13 01:34:45 +0200

    popup

--- app/layout.js
+++ app/layout.js
@@ -8,9 +8,21 @@ import Popup from "./components/Popup";
 import "./globals.scss";
 import Script from "next/script";
 import "@/public/styles/utilities/variables.scss";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "gtag"
 
 const RootLayout = ({ children }) => {
-  const NEXT_PUBLIC_GOOGLE_ANALYTICS = "G-CYPLVVSN8B";
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
   
   return (
     <html lang="en" className="main">
@@ -73,20 +85,23 @@ const RootLayout = ({ children }) => {
           />
         </noscript>
         <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-
-        <Script strategy="lazyOnload">
-          {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-        </Script>
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       </Head>
 
       <body>
